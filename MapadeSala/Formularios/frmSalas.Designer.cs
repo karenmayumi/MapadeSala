b4c592294bd93231d829c82517101aa528e320ef
Samuel Seven <sevenoffice777@gmail.com>
2024-05-28 09:35:22 -0300

    Adicionando form Sala e ligando com o menu do Principal

---
+++ MapadeSala/Formularios/frmSalas.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace MapadeSala.Formularios
+{
+    partial class frmSalas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumComp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumChair = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkIsLab = new System.Windows.Forms.CheckBox();
+            this.chkIsAvaliable = new System.Windows.Forms.CheckBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.dtGridSalas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridSalas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(36, 65);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(63, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(115, 65);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome da sala";
+            // 
+            // txtNumComp
+            // 
+            this.txtNumComp.Location = new System.Drawing.Point(230, 65);
+            this.txtNumComp.Name = "txtNumComp";
+            this.txtNumComp.Size = new System.Drawing.Size(100, 20);
+            this.txtNumComp.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(229, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Num Comput";
+            // 
+            // txtNumChair
+            // 
+            this.txtNumChair.Location = new System.Drawing.Point(346, 65);
+            this.txtNumChair.Name = "txtNumChair";
+            this.txtNumChair.Size = new System.Drawing.Size(100, 20);
+            this.txtNumChair.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Num Chairs";
+            // 
+            // chkIsLab
+            // 
+            this.chkIsLab.AutoSize = true;
+            this.chkIsLab.Location = new System.Drawing.Point(459, 59);
+            this.chkIsLab.Name = "chkIsLab";
+            this.chkIsLab.Size = new System.Drawing.Size(85, 17);
+            this.chkIsLab.TabIndex = 8;
+            this.chkIsLab.Text = "É laboratório";
+            this.chkIsLab.UseVisualStyleBackColor = true;
+            // 
+            // chkIsAvaliable
+            // 
+            this.chkIsAvaliable.AutoSize = true;
+            this.chkIsAvaliable.Location = new System.Drawing.Point(550, 59);
+            this.chkIsAvaliable.Name = "chkIsAvaliable";
+            this.chkIsAvaliable.Size = new System.Drawing.Size(101, 17);
+            this.chkIsAvaliable.TabIndex = 9;
+            this.chkIsAvaliable.Text = "Está Disponível";
+            this.chkIsAvaliable.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(682, 59);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 10;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // dtGridSalas
+            // 
+            this.dtGridSalas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridSalas.Location = new System.Drawing.Point(27, 103);
+            this.dtGridSalas.Name = "dtGridSalas";
+            this.dtGridSalas.Size = new System.Drawing.Size(727, 303);
+            this.dtGridSalas.TabIndex = 11;
+            // 
+            // frmSalas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtGridSalas);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.chkIsAvaliable);
+            this.Controls.Add(this.chkIsLab);
+            this.Controls.Add(this.txtNumChair);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNumComp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSalas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmSalas";
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridSalas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumComp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumChair;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkIsLab;
+        private System.Windows.Forms.CheckBox chkIsAvaliable;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.DataGridView dtGridSalas;
+    }
+}
--- MapadeSala/Principal.cs
+++ MapadeSala/Principal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MapadeSala.Formularios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,60 +18,11 @@ namespace MapadeSala
             InitializeComponent();
         }
 
-        private void btnHelloWorld_click(object sender, EventArgs e)
+
+        private void salasToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world!");
-        }
-
-        private void Principal_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnHelloWorld_enter(object sender, EventArgs e)
-        {
-            MessageBox.Show("Entrando com o mouse no botão");
-
-        }
-
-        private void btn_ViewName(object sender, EventArgs e)
-        {
-            string nome = txt_n1.Text;
-            MessageBox.Show(nome);
-        }
-        
-        private void btn_Sum_Somar(object sender, EventArgs e)
-        {
-            int n1 = Convert.ToInt32(txt_n1.Text);
-            int n2 = Convert.ToInt32(txt_n2.Text);
-            int soma = n1 + n2;
-            MessageBox.Show(Convert.ToString(soma));
-        }
-
-        private void btn_Sub_Subtrair(object sender, EventArgs e)
-        {
-            int n1 = Convert.ToInt32(txt_n1.Text);
-            int n2 = Convert.ToInt32(txt_n2.Text);
-            int subtracao = n1 - n2;
-            MessageBox.Show(Convert.ToString(subtracao));
-
-        }
-
-        private void btn_Multi_Multiplicar(object sender, EventArgs e)
-        {
-            int n1 = Convert.ToInt32(txt_n1.Text);
-            int n2 = Convert.ToInt32(txt_n2.Text);
-            int multiplicacao = n1 * n2;
-            MessageBox.Show(Convert.ToString(multiplicacao));
-        }
-
-        private void btn_Div_Dividir(object sender, EventArgs e)
-        {
-            int n1 = Convert.ToInt32(txt_n1.Text);
-            int n2 = Convert.ToInt32(txt_n2.Text);
-            int divisao = n1 / n2;
-            MessageBox.Show(Convert.ToString(divisao));
-
+            frmSalas s = new frmSalas();
+            s.ShowDialog();
         }
     }
 }
